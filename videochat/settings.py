6e107ed2c87d0ace5d27a986c8014149e8e85619
Mansur Ahmed <shaikmansoor586@gmail.com>
2024-09-12 23:32:08 +0530

    yesssir

--- videochat/settings.py
+++ videochat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=^80&ggga5pecfx(-x&9f3m*&%=j3c-jgf+8voe5fhky0g%b3k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
